c86c282bcc27ee6459553dabc6a457f6c8532abe
Umar-azeem <umara4436@gmail.com>
2026-04-15 20:26:09 +0500

    UI changes

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,7 +1,8 @@
 // src/app/layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
+// import "./global.css";
+
 import Header from "@/src/components/header";
 import { AuthProvider } from "@/src/context/AuthContext";
 
